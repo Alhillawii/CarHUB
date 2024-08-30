cb3477ae3f0ed877198e12f75b758c956de87ee4
Alhillawii <ayahhillawi9@gmail.com>
2024-08-30 15:04:47 +0300

    migrate

---
+++ app/Http/Controllers/TesteimonialController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Testeimonial;
+use Illuminate\Http\Request;
+
+class TesteimonialController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Testeimonial $testeimonial)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Testeimonial $testeimonial)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Testeimonial $testeimonial)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Testeimonial $testeimonial)
+    {
+        //
+    }
+}
